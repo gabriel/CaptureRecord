f88a23afdaf471fd86671e5cd9c446248da7554c
Gabriel Handford <gabrielh@gmail.com>
2012-10-23 14:08:06 -0700

    Adding commercial license

--- CaptureRecord/CaptureRecord/CRUtils.h
+++ CaptureRecord/CaptureRecord/CRUtils.h
@@ -2,8 +2,25 @@
 //  CRUtils.h
 //  CaptureRecord
 //
-//  Created by Gabriel Handford on 8/29/12.
+//  Created by Miscellaneum LLC on 8/29/12.
 //  Copyright (c) 2012 Miscellaneum LLC. All rights reserved.
+//
+//  Licensed under the Cocoa Controls commercial license agreement, v1, included
+//  with the original package and can also be found at:
+//  <http://CocoaControls.com/licenses/v1/license.pdf>.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 @interface CRUtils : NSObject
